b9e9873850291649938cb25ca98099c3da65091a
Data-Wizad <healthanalyst7@gmail.com>
2026-09-21 17:40:24 +0000

    logging and exception

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import find_packages,setup
 from typing import list
 HYPEN_E_DOT='-e .'
-def get_requirement(file_path:str)->list[str]:
+def get_requirements(file_path:str)->list[str]:
     ...
     # this function will return the list of requirement
     ...
-    requirement=[]
+    requirements=[]
     with open(file_path) as file_obj:
         requirement=file_obj.readlines()
         requirements=[req.replace ("\n" ,"") for req in requirement.txt]
-        if HYPEN_E_DOT in requirement:
-            requirement.remove(HYPEN_E_DOT) 
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT) 
     return requirement
 
 setup (
@@ -19,6 +19,6 @@ setup (
     author="Data Tycoon",
     author_email="healthanalyst7@gmail.com",
     packages=find_packages(),
-    install_requires=get_requirement('requirement.txt')
+    install_requires=get_requirements('requirement.txt')
     
 )
